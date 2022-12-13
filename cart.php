75cc64eb6d3cc8d82a8deb16c0c388cc62d3621f
Daniyal khan <dk720820@gmail.com>
2022-12-13 21:21:05 +0500

    changes

--- cart.php
+++ cart.php
@@ -1,8 +1,11 @@
 <?php
+
 $page = "cart";
 //require header file
 require('header.php');
-
+if(!isset($_SESSION['email'])){
+  echo "<script>window.location.href='login.php'</script>";
+}
 // require functions file
 require_once('functions.php');
 ?>
@@ -15,16 +18,8 @@ require_once('functions.php');
 
   if (isset($_POST['checkout'])) {
 
-
-    //Stablishing Connection...
+    include('smtp/PHPMailerAutoload.php');
     include 'db.php';
-
-    // $user = $_SESSION['email'];
-
-    
-    // $query = "SELECT * FROM `users` where email = '$user'";
-    // $res = mysqli_query($con, $query);
-    // $row = mysqli_fetch_assoc($res);
 
     //Getting Values From Form Tag...
     $user_id = $_POST['user_id'];
@@ -34,7 +29,6 @@ require_once('functions.php');
     $phone = $_POST['phone'];
     $address = $_POST['address'];
     $price = $_POST['price'];
-
 
     //Insert Query For Mysql..
     $query = "INSERT INTO `orders`(`user_id`, `product_id`, `tracking_id`, `quantity`,`price`,`address`)
@@ -51,19 +45,126 @@ require_once('functions.php');
 
       foreach($_SESSION['cart'] as $item){
         
-        $name = $item['name'];
-        $product_image = $item['product_image'];
-        $detail = $item['detail'];
-        $quantity = $item['quantity'];
-        $price = $item['price'];
+        $name1 = $item['name'];
+        $product_image1 = $item['product_image'];
+        $detail1 = $item['detail'];
+        $quantity1 = $item['quantity'];
+        $price1 = $item['price'];
 
         //Insert Query For Mysql..
         $query = "INSERT INTO `order_detail`(`order_id`, `name`, `image`, `detail`, `quantity`, `price`) 
-        VALUES ('$order_id','$name','$product_image','$detail','$quantity','$price')";
+        VALUES ('$order_id','$name1','$product_image1','$detail1','$quantity1','$price1')";
         $res = mysqli_query($con, $query);
-        
-      
       }
+
+
+$html='<html>
+<body style="background-color:#e2e1e0;font-family: Open Sans, sans-serif;font-size:100%;font-weight:400;line-height:1.4;color:#000;">
+  <table style="max-width:670px;margin:50px auto 10px;background-color:#fff;padding:50px;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;-webkit-box-shadow:0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24);-moz-box-shadow:0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24);box-shadow:0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24); border-top: solid 10px red;">
+    <thead>
+      <tr>
+        <th style="text-align:left;"><img style="max-width: 150px;" src="https://expressmall.store/icon.jpeg" alt="Express Mall Store"></th>
+        <th style="text-align:right;font-weight:400;">';
+
+        $date = date("Y-m-d");
+        $newDate = date('F', strtotime($date));
+  
+        $time = '19:24:15'; 
+        $time = date('h:i:s', strtotime($date));
+
+$html .= date("d")." ".$newDate[0].$newDate[1].$newDate[2]." ".date("y")."<br>".$time;
+
+$html .= '</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td style="height:35px;"></td>
+      </tr>
+      <tr>
+        <td colspan="2" style="border: solid 1px #ddd; padding:10px 20px;">
+          <p style="font-size:14px;margin:0 0 6px 0;"><span style="font-weight:bold;display:inline-block;min-width:150px">Order status</span><b style="color:green;font-weight:normal;margin:0">Success</b></p>
+          <p style="font-size:14px;margin:0 0 6px 0;"><span style="font-weight:bold;display:inline-block;min-width:146px">Tracking ID</span>'; $html .= $tracking_id; 
+          $html .='</p>
+          <p style="font-size:14px;margin:0 0 0 0;"><span style="font-weight:bold;display:inline-block;min-width:146px">Order amount</span>'; $html .= $price; 
+          $html .='</p>
+        </td>
+      </tr>
+      <tr>
+        <td style="height:35px;"></td>
+      </tr>
+      <tr>
+        <td style="width:50%;padding:20px;vertical-align:top">
+          <p style="margin:0 0 10px 0;padding:0;font-size:14px;"><span style="display:block;font-weight:bold;font-size:13px">Name</span>'; $html .= $name; 
+          $html .='</p>
+          <p style="margin:0 0 10px 0;padding:0;font-size:14px;"><span style="display:block;font-weight:bold;font-size:13px;">Email</span> '; $html .= $email; 
+          $html .='</p>
+          <p style="margin:0 0 10px 0;padding:0;font-size:14px;"><span style="display:block;font-weight:bold;font-size:13px;">Phone</span> '; $html .= $phone; 
+          $html .='</p>
+        </td>
+        <td style="width:50%;padding:20px;vertical-align:top">
+          <p style="margin:0 0 10px 0;padding:0;font-size:14px;"><span style="display:block;font-weight:bold;font-size:13px;">Address</span>'; $html .= $address; 
+          $html .='</p>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2" style="font-size:20px;padding:30px 15px 0 15px;">Items</td>
+      </tr>
+      <tr>
+        <td colspan="2" style="padding:15px;">
+          <p style="font-size:14px;margin:0;padding:10px;border:solid 1px #ddd;font-weight:bold;">
+            <span style="display:block;font-size:13px;font-weight:normal;">Homestay with fooding & lodging</span> Rs. 3500 <b style="font-size:12px;font-weight:300;"> /person/day</b>
+          </p>
+          <p style="font-size:14px;margin:0;padding:10px;border:solid 1px #ddd;font-weight:bold;"><span style="display:block;font-size:13px;font-weight:normal;">Pickup & Drop</span> Rs. 2000 <b style="font-size:12px;font-weight:300;"> /person/day</b></p>
+          <p style="font-size:14px;margin:0;padding:10px;border:solid 1px #ddd;font-weight:bold;"><span style="display:block;font-size:13px;font-weight:normal;">Local site seeing with guide</span> Rs. 800 <b style="font-size:12px;font-weight:300;"> /person/day</b></p>
+          <p style="font-size:14px;margin:0;padding:10px;border:solid 1px #ddd;font-weight:bold;"><span style="display:block;font-size:13px;font-weight:normal;">Tea tourism with guide</span> Rs. 500 <b style="font-size:12px;font-weight:300;"> /person/day</b></p>
+          <p style="font-size:14px;margin:0;padding:10px;border:solid 1px #ddd;font-weight:bold;"><span style="display:block;font-size:13px;font-weight:normal;">River side camping with guide</span> Rs. 1500 <b style="font-size:12px;font-weight:300;"> /person/day</b></p>
+          <p style="font-size:14px;margin:0;padding:10px;border:solid 1px #ddd;font-weight:bold;"><span style="display:block;font-size:13px;font-weight:normal;">Trackking and hiking with guide</span> Rs. 1000 <b style="font-size:12px;font-weight:300;"> /person/day</b></p>
+        </td>
+      </tr>
+    </tbody>
+    <tfooter>
+      <tr>
+        <td colspan="2" style="font-size:14px;padding:50px 15px 0 15px;">
+          <strong style="display:block;margin:0 0 10px 0;">Regards</strong> Express Mall Store<br> Karachi, Pakistan<br><br>
+          <b>Phone:</b> 03552-222011<br>
+          <b>Email:</b> support@expressmall.store
+        </td>
+      </tr>
+    </tfooter>
+  </table>
+</body>
+
+</html>';
+
+smtp_mailer('daniyalreavtech@gmail.com','Express Mall Store | Purchase Invoice',$html);
+function smtp_mailer($to,$subject, $msg){
+	$mail = new PHPMailer(); 
+	$mail->SMTPDebug  = 3;
+	$mail->IsSMTP(); 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls'; 
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->Username = "daniyalreavtech@gmail.com";
+	$mail->Password = "jswairgfcbxpomhw";
+	$mail->SetFrom("daniyalreavtech@gmail.com");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+	if(!$mail->Send()){
+		echo $mail->ErrorInfo;
+	}else{
+		return 'Sent';
+	}
+}
 
       unset($_SESSION['cart']);
       echo "<script>alert('Your order has been confirmed!')</script>";
