35d8307ae7d765a8cc27fee1793137725d34f9f0
Daniyal khan <dk720820@gmail.com>
2022-12-13 20:47:56 +0500

    changes

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php 
- $host = 'localhost';
- $user = 'root';
- $password = '';
- $database = 'ecom';
-
 //  $host = 'localhost';
-//  $user = 'u343233411_expressmall';
-//  $password = 'expressmallStore2020@';
-//  $database = 'u343233411_expressmall';
+//  $user = 'root';
+//  $password = '';
+//  $database = 'ecom';
+
+ $host = 'localhost';
+ $user = 'u343233411_expressmall';
+ $password = 'expressmallStore2020@';
+ $database = 'u343233411_expressmall';
 
 $con = mysqli_connect($host, $user, $password, $database) or die("Query Failed!!!");
 
