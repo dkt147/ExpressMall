b5dd686eaebda53958173997c2b7353584aebeb9
Daniyal khan <dk720820@gmail.com>
2022-12-13 21:33:06 +0500

    changes

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ include('smtp/PHPMailerAutoload.php');
 
 $html='<html>
 <body style="background-color:#e2e1e0;font-family: Open Sans, sans-serif;font-size:100%;font-weight:400;line-height:1.4;color:#000;">
-  <table style="max-width:670px;margin:50px auto 10px;background-color:#fff;padding:50px;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;-webkit-box-shadow:0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24);-moz-box-shadow:0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24);box-shadow:0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24); border-top: solid 10px green;">
+  <table style="max-width:670px;margin:50px auto 10px;background-color:#fff;padding:50px;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;-webkit-box-shadow:0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24);-moz-box-shadow:0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24);box-shadow:0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24); border-top: solid 10px red;">
     <thead>
       <tr>
         <th style="text-align:left;"><img style="max-width: 150px;" src="https://expressmall.store/icon.jpeg" alt="Express Mall Store"></th>
@@ -28,8 +28,12 @@ $html .= '</th>
       <tr>
         <td colspan="2" style="border: solid 1px #ddd; padding:10px 20px;">
           <p style="font-size:14px;margin:0 0 6px 0;"><span style="font-weight:bold;display:inline-block;min-width:150px">Order status</span><b style="color:green;font-weight:normal;margin:0">Success</b></p>
-          <p style="font-size:14px;margin:0 0 6px 0;"><span style="font-weight:bold;display:inline-block;min-width:146px">Tracking ID</span> abcd1234567890</p>
-          <p style="font-size:14px;margin:0 0 0 0;"><span style="font-weight:bold;display:inline-block;min-width:146px">Order amount</span> Rs. 6000.00</p>
+          <p style="font-size:14px;margin:0 0 6px 0;"><span style="font-weight:bold;display:inline-block;min-width:146px">Tracking ID</span>'; 
+          $html .= $tracking_id; 
+          $html .='</p>
+          <p style="font-size:14px;margin:0 0 0 0;"><span style="font-weight:bold;display:inline-block;min-width:146px">Order amount</span>'; 
+          $html .= "Rs/- ".$price; 
+          $html .='</p>
         </td>
       </tr>
       <tr>
@@ -37,30 +41,41 @@ $html .= '</th>
       </tr>
       <tr>
         <td style="width:50%;padding:20px;vertical-align:top">
-          <p style="margin:0 0 10px 0;padding:0;font-size:14px;"><span style="display:block;font-weight:bold;font-size:13px">Name</span> Palash Basak</p>
-          <p style="margin:0 0 10px 0;padding:0;font-size:14px;"><span style="display:block;font-weight:bold;font-size:13px;">Email</span> palash@gmail.com</p>
-          <p style="margin:0 0 10px 0;padding:0;font-size:14px;"><span style="display:block;font-weight:bold;font-size:13px;">Phone</span> +91-1234567890</p>
+          <p style="margin:0 0 10px 0;padding:0;font-size:14px;"><span style="display:block;font-weight:bold;font-size:13px">Name</span>';
+          $html .= $name; 
+          $html .='</p>
+          <p style="margin:0 0 10px 0;padding:0;font-size:14px;"><span style="display:block;font-weight:bold;font-size:13px;">Email</span> '; 
+          $html .= $email; 
+          $html .='</p>
+          <p style="margin:0 0 10px 0;padding:0;font-size:14px;"><span style="display:block;font-weight:bold;font-size:13px;">Phone</span> '; 
+          $html .= $phone; 
+          $html .='</p>
         </td>
         <td style="width:50%;padding:20px;vertical-align:top">
-          <p style="margin:0 0 10px 0;padding:0;font-size:14px;"><span style="display:block;font-weight:bold;font-size:13px;">Address</span> Khudiram Pally, Malbazar, West Bengal, India, 735221</p>
+          <p style="margin:0 0 10px 0;padding:0;font-size:14px;"><span style="display:block;font-weight:bold;font-size:13px;">Address</span>'; 
+          $html .= $address; 
+          $html .='</p>
         </td>
       </tr>
       <tr>
         <td colspan="2" style="font-size:20px;padding:30px 15px 0 15px;">Items</td>
-      </tr>
-      <tr>
-        <td colspan="2" style="padding:15px;">
+      </tr><tr>
+      <td colspan="2" style="padding:15px;">';
+
+      foreach($_SESSION['cart'] as $item){
+
+      $html .= '
           <p style="font-size:14px;margin:0;padding:10px;border:solid 1px #ddd;font-weight:bold;">
-            <span style="display:block;font-size:13px;font-weight:normal;">Homestay with fooding & lodging</span> Rs. 3500 <b style="font-size:12px;font-weight:300;"> /person/day</b>
-          </p>
-          <p style="font-size:14px;margin:0;padding:10px;border:solid 1px #ddd;font-weight:bold;"><span style="display:block;font-size:13px;font-weight:normal;">Pickup & Drop</span> Rs. 2000 <b style="font-size:12px;font-weight:300;"> /person/day</b></p>
-          <p style="font-size:14px;margin:0;padding:10px;border:solid 1px #ddd;font-weight:bold;"><span style="display:block;font-size:13px;font-weight:normal;">Local site seeing with guide</span> Rs. 800 <b style="font-size:12px;font-weight:300;"> /person/day</b></p>
-          <p style="font-size:14px;margin:0;padding:10px;border:solid 1px #ddd;font-weight:bold;"><span style="display:block;font-size:13px;font-weight:normal;">Tea tourism with guide</span> Rs. 500 <b style="font-size:12px;font-weight:300;"> /person/day</b></p>
-          <p style="font-size:14px;margin:0;padding:10px;border:solid 1px #ddd;font-weight:bold;"><span style="display:block;font-size:13px;font-weight:normal;">River side camping with guide</span> Rs. 1500 <b style="font-size:12px;font-weight:300;"> /person/day</b></p>
-          <p style="font-size:14px;margin:0;padding:10px;border:solid 1px #ddd;font-weight:bold;"><span style="display:block;font-size:13px;font-weight:normal;">Trackking and hiking with guide</span> Rs. 1000 <b style="font-size:12px;font-weight:300;"> /person/day</b></p>
-        </td>
-      </tr>
-    </tbody>
+            <span style="display:block;font-size:13px;font-weight:normal;">';$html .= $item['name']; 
+            $html .='</span> Rs.';$html .= $item['price']; 
+            $html .='<b style="font-size:12px;font-weight:300;"></b>
+          </p>';
+   
+    }
+
+    $html .= '
+    </td>
+</tr></tbody>
     <tfooter>
       <tr>
         <td colspan="2" style="font-size:14px;padding:50px 15px 0 15px;">
@@ -72,8 +87,8 @@ $html .= '</th>
     </tfooter>
   </table>
 </body>
-
 </html>';
+
 
 smtp_mailer('daniyalreavtech@gmail.com','Express Mall Store | Purchase Invoice',$html);
 function smtp_mailer($to,$subject, $msg){
@@ -98,9 +113,8 @@ function smtp_mailer($to,$subject, $msg){
 		'allow_self_signed'=>false
 	));
 	if(!$mail->Send()){
-		echo $mail->ErrorInfo;
+		return 0;
 	}else{
-		return 'Sent';
+		return 1;
 	}
 }
-?>
